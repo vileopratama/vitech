efedab682457609be52026b3cda3d7f255cc8caf
suhe <hendarsyahss@gmail.com>
2017-01-09 13:57:51 +0700

    custom backend theme

---
+++ src/addons/vitech_backend_theme/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name' : 'Vitech Backend Theme for Odoo 9c',
+    'version' : '0.3',
+    'author' : 'Vileo Pratama Teknologi',
+    'category' : 'Website',
+    'summary': 'Clean Theme for Odoo 9c',
+    'website' : 'http://www.vileo.co.id',
+    'description': """
+Clean Theme Odoo 9c for Vitech ERP, based on Bootstrap United template. The theme also custom every time from bugs.
+    """,
+    'images':[
+        'images/sales.png'
+    ],
+    'depends' : ['base'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
