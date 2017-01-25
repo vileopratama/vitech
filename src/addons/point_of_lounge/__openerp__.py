f95d0dc5cd7db7a3af00f70dff1a8e092fb06dfe
suhe <hendarsyahss@gmail.com>
2017-01-25 17:15:24 +0700

    update lounge

--- src/addons/point_of_lounge/__openerp__.py
+++ src/addons/point_of_lounge/__openerp__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Point Of Lounge",
-    'summary': "Short subtitle phrase",
-    'description': """Long description""",
+    'summary': "Point of Lounge",
+    'description': """Sale Up your """,
     'author': "Suhendar",
     'license': "AGPL-3",
-    'website': "http://www.example.com",
+    'website': "http://www.vileo.co.id",
     'category': 'Sales',
     'version': '9.0.1.0.0',
     'depends': ['sale_stock', 'barcodes','account','account_accountant'],
@@ -19,6 +19,7 @@
         'point_of_lounge_report.xml',
         'point_of_lounge_view.xml',
         'point_of_lounge_data.xml',
+        'point_of_lounge_sequence.xml',
         'point_of_lounge_workflow.xml',
         'account_statement_view.xml',
         'res_config_view.xml',
